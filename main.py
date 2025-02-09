53644b473fcee5ccd6c8d82253b404864308eeac
Brandon <Brandon.f.gruber@gmail.com>
2025-02-09 21:45:09 +0000

    add object groupings

--- main.py
+++ main.py
@@ -8,22 +8,30 @@ def main():
     
     # Create clock
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     # Create player
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
+    dt = 0
     # Game loop.
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
+        updatable.update(dt)
 
         # Render        
         screen.fill(color="black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
 
         # Limit FPS to 60.
