699dae59806a65180952cd6c7404a818fb220669
Brandon <Brandon.f.gruber@gmail.com>
2025-02-09 18:20:49 +0000

    add clock functionality

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  
     
+    # Create clock
+    clock = pygame.time.Clock()
+    dt = 0
+
     # Game loop.
     while True:
         for event in pygame.event.get():
@@ -12,6 +16,9 @@ def main():
                 return
         screen.fill(color="black")
         pygame.display.flip()
+
+        # Limit FPS to 60.
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
